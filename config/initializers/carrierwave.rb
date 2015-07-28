067b6ae984a93200b06feb4144caa79777135772
Maren Woodruff <wren403@gmail.com>
2015-07-28 17:23:07 -0400

    fixed carrierwave provider

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-  	:PROVIDER =>              'AWS',                        # required
+  	:provider =>              'AWS',                        # required
     :AWS_ACCESS_KEY_ID =>     ENV["AWS_ACCESS_KEY_ID"],                        # required
     :AWS_SECRET_ACCESS_KEY => ENV["AWS_SECRET_ACCESS_KEY"],
     :REGION => 								ENV["REGION"]
