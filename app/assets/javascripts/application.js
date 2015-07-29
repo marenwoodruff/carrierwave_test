41d4682858956eb6f6b65108980ac7ad9a5685ee
Maren Woodruff <wren403@gmail.com>
2015-07-29 13:56:12 -0400

    added first_name and last_name to user. added fields to devise. added admin boolean.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require turbolinks
 //= require jquery-fileupload/basic
 //= require jquery-fileupload/vendor/tmpl
-//= require turbolinks
 //= require_tree .
